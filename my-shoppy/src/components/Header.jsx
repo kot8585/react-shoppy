8fb16998ea1f87138026188a23987a06395835b1
kot8585 <ansgywjd321@gmail.com>
2023-02-15 10:36:10 +0900

    "my" - Header 골격 잡기

--- my-shoppy/src/components/Header.jsx
+++ my-shoppy/src/components/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { TbBuildingStore } from 'react-icons/tb';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div>
-      헤더
-    </div>
+    <header className='flex justify-between items-center text-lg'>
+      <Link to="/" className='flex items-center text-main gap-2'>
+        <TbBuildingStore/>
+        <span>Shoppy</span>
+      </Link>
+      <div className='flex items-center gap-2'>
+        <Link to="products">products</Link>
+        {/* TODO: 로그인 되어있지 않을경우 redirect 하기 */}
+        <AiOutlineShoppingCart /> 
+        <button className='bg-main text-white px-2'>Login</button>
+      </div>
+    </header>
   );
 }
 
