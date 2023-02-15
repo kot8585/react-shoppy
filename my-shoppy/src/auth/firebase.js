76ab16441a2ec781b00fd314dc9ae63b5cc68ccb
kot8585 <ansgywjd321@gmail.com>
2023-02-15 16:05:05 +0900

    "my" - 일반 사용자 로그인, 로그아웃

---
+++ my-shoppy/src/auth/firebase.js
@@ -0,0 +1,41 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider(); 
+
+export async function login() { 
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        console.log("token", token);
+        
+        return result.user;        
+      }).catch((error) => {
+        throw new Error(`login error : ${error}`)
+      });
+  }
+
+  export function getUserState(callback) {
+    onAuthStateChanged(auth, callback);
+}
+
+export async function logout() {
+  signOut(auth).then(() => {
+      return null;
+    }).catch((error) => {
+      throw new Error(`logout error: ${error}`)
+    });
+  }
+export {auth};
--- my-shoppy/src/components/Header.jsx
+++ my-shoppy/src/components/Header.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TbBuildingStore } from 'react-icons/tb';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import { login, logout, getUserState } from '../auth/firebase';
 
 export default function Header() {
+  const [user, setUser] = useState(null); 
+
+  const handleClick = () => {
+    if(!user){
+      login().then((user) => setUser(user));
+    } else {
+      logout().then(() => setUser(null));
+      console.log('로그아웃 되었음')
+    }
+  }
+
+  useEffect(() => {
+    getUserState((user) => {setUser(user)});
+  }, []);
+
+  console.log('user state', user);
+
   return (
     <header className='flex justify-between items-center text-lg'>
       <Link to="/" className='flex items-center text-main gap-2'>
@@ -14,7 +32,21 @@ export default function Header() {
         <Link to="products">products</Link>
         {/* TODO: 로그인 되어있지 않을경우 redirect 하기 */}
         <AiOutlineShoppingCart /> 
-        <button className='bg-main text-white px-2'>Login</button>
+        {user && 
+        <div className='flex items-center'>
+          <img 
+            className='w-7 h-7 rounded-full' 
+            src={user.photoURL}
+            alt='프로필 이미지'
+            referrerPolicy='no-referrer'
+            />
+            <span className='text-sm'>{user.displayName}</span>
+        </div>
+        }
+        <button 
+          className='bg-main text-white px-2'
+          onClick={handleClick}
+        >{user ? 'Logout' : 'Login'}</button>
       </div>
     </header>
   );
