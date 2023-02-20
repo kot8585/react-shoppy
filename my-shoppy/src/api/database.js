95228b6f5798396066e932bc652dabaf203666c5
kot8585 <ansgywjd321@gmail.com>
2023-02-20 16:42:04 +0900

    파일명 database에서 api로 변경, uploadImg를 api 폴더로 옮김.
    uploadImg같은거 컴포넌트에 넣는거 아니래. 비즈니스 로직말이야

---
+++ my-shoppy/src/api/database.js
@@ -0,0 +1,41 @@
+import { getDatabase, ref, child, get, set } from "firebase/database";
+import { app } from "./config";
+import {v4 as uuidv4} from 'uuid';
+
+const database = getDatabase(app);
+
+const dbRef = ref(getDatabase());
+export function checkIsAdmin (user) {
+  return get(child(dbRef, `admins/`)).then((snapshot) => {
+    if (snapshot.exists()) {
+        return snapshot.val().includes(user.uid);
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+}
+
+export async function getData(callback) {
+  return get(child(dbRef, `products/`)).then((snapshot) => {
+    if (snapshot.exists()) {
+        callback(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+}
+
+export async function writeProductData(product, imageUrl) {
+  const id = uuidv4();
+  const db = getDatabase();
+  return set(ref(db, 'products/'+id),{
+    ...product, 
+      imageUrl, 
+      id: id,
+      option: product.option.split(',')
+    });
+}
---
+++ my-shoppy/src/api/uploader.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export async function uploadImage(imageUrl) {
+  const cloudName = 'dxy2ifpy4';
+  const resourceType = 'image'; 
+  const url = `https://api.cloudinary.com/v1_1/${cloudName}/${resourceType}/upload`;
+
+  const formData = new FormData();
+  formData.append('file', imageUrl);
+  formData.append("upload_preset", process.env.REACT_APP_CLOUDINARY_PRESET);
+
+  return axios.post(url, formData)
+  .then((response) => {
+      return response.data;
+    })
+  .catch((error) => console.error(error));
+}
